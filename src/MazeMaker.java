f1cb2abbe1f74994e83b6c25fa12f1ef0f8a349a
Isaacpants <league@WTS-IM-17.attlocal.net>
2018-06-27 16:57:43 -0700

    FIGURE OUT HOW TO DETERMINE WHICH WALL IS BETWEEN THE TWO CELLS

--- src/MazeMaker.java
+++ src/MazeMaker.java
@@ -20,6 +20,8 @@ public class MazeMaker{
 		maze = new Maze(width, height);
 		
 		//select a random cell to start
+		Cell random = maze.getCell(randGen.nextInt(width), randGen.nextInt(height));
+		selectNextPath(random);
 		
 		//call selectNextPath method with the randomly selected cell
 		
@@ -28,8 +30,41 @@ public class MazeMaker{
 
 	private static void selectNextPath(Cell currentCell) {
 		// mark current cell as visited
+		currentCell.setBeenVisited(true);
 
 		// check for unvisited neighbors
+		if(currentCell.getX() == 0) {
+			currentCell.setNorthWall(false);
+		}
+		if(currentCell.getY() == 0) {
+			currentCell.setWestWall(false);
+		}
+		if(currentCell.getX() == 4) {
+			currentCell.setSouthWall(false);
+		}
+		if(currentCell.getY() == 4) {
+			currentCell.setEastWall(false);
+		}
+		ArrayList<Cell> unvisitedNeighbors = new ArrayList<Cell>();
+		for (int i = 0; i < 4; i++) {
+			if(i == 0) {
+				if(currentCell.hasNorthWall() == true) {
+					unvisitedNeighbors.add(maze.getCell(currentCell.getX(), currentCell.getY()-1));
+				}
+				if(currentCell.hasEastWall() == true) {
+					unvisitedNeighbors.add(maze.getCell(currentCell.getX()+1, currentCell.getY()));
+				}
+				if(currentCell.hasSouthWall() == true) {
+					unvisitedNeighbors.add(maze.getCell(currentCell.getX(), currentCell.getY()+1));
+				}
+				if(currentCell.hasWestWall() == true) {
+					unvisitedNeighbors.add(maze.getCell(currentCell.getX()-1, currentCell.getY()));
+				}
+			}
+		}
+		Cell chosenCell = unvisitedNeighbors.get(randGen.nextInt(unvisitedNeighbors.size()));
+		uncheckedCells.push(chosenCell);
+		//NOTE TO SELF: FIGURE OUT HOW TO DETERMINE WHICH WALL IS BETWEEN THE TWO CELLS
 		
 		// if has unvisited neighbors,
 			// select one at random.
